f27752626a94b09e0fe9bf18d529229331c3a291
Jason McNeil <sixcolors@mac.com>
2021-02-25 21:38:10 -0400

    fix docker name

--- deploy.sh
+++ deploy.sh
@@ -4,9 +4,9 @@ docker push mjsouthcott/safety-zone
 
 ssh deploy@$DEPLOY_SERVER << EOF
 docker pull mjsouthcott/safety-zone
-docker stop api-saftey-zone || true
-docker rm api-saftey-zone || true
+docker stop saftey-zone || true
+docker rm saftey-zone || true
 docker rmi mjsouthcott/safety-zone:current || true
 docker tag mjsouthcott/safety-zone:latest mjsouthcott/safety-zone:current
-docker run -d --restart always --name api-saftey-zone -p 3000:3000 mjsouthcott/safety-zone:current
+docker run -d --restart always --name saftey-zone -p 3000:3000 mjsouthcott/safety-zone:current
 EOF
